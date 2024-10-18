ea5470c64ca29e575bebc1fa624151b26bcdf727
Onur <onur.kayhan90@gmail.com>
2024-10-18 13:54:23 +0200

    Initial Commit

---
+++ FaveAndRate/FaveAndRateApp.swift
@@ -0,0 +1,17 @@
+//
+//  FaveAndRateApp.swift
+//  FaveAndRate
+//
+//  Created by Onur Kayhan on 2024-10-18.
+//
+
+import SwiftUI
+
+@main
+struct FaveAndRateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
