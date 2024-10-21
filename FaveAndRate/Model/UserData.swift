0dca021f1c557abdbb1f5e68dbae1a980f59b775
hampus-code <hampusss96@gmail.com>
2024-10-21 12:34:42 +0200

    Feature: Add Model folder and codeable UserData struct

---
+++ FaveAndRate/Model/UserData.swift
@@ -0,0 +1,15 @@
+//
+//  UserData.swift
+//  FaveAndRate
+//
+//  Created by Hampus Andersson on 2024-10-21.
+//
+
+import Foundation
+
+struct UserData: Codable {
+    
+    var name: String
+    var watchlist: [String]
+    
+}
