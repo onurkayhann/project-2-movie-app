a0259b803edeb41e5dfc77304114b7e54e7a0128
Onur <onur.kayhan90@gmail.com>
2024-10-21 11:12:02 +0200

    Feature: Add ViewModel folder, DbConnection file, and FirebaseFirestore

---
+++ FaveAndRate/ViewModel/DbConnection.swift
@@ -0,0 +1,16 @@
+//
+//  DbConnection.swift
+//  FaveAndRate
+//
+//  Created by Onur Kayhan on 2024-10-21.
+//
+
+import Foundation
+import FirebaseFirestore
+
+class DbConnection: ObservableObject {
+    
+    var db = Firestore.firestore()
+    
+    
+}
