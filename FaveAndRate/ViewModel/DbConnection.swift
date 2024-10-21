a1c6003260bbd730be631e0d4f50208ad37b6ae8
Onur <onur.kayhan90@gmail.com>
2024-10-21 13:19:17 +0200

    Feature: Add registration function in DbConnection

--- FaveAndRate/ViewModel/DbConnection.swift
+++ FaveAndRate/ViewModel/DbConnection.swift
@@ -7,12 +7,39 @@
 
 import Foundation
 import FirebaseFirestore
+import FirebaseAuth
 
 class DbConnection: ObservableObject {
     
     var db = Firestore.firestore()
+    var auth = Auth.auth()
     
     let COLLECTION_USER_DATA = "user_data"
+    
+    func registerUser(email: String, password: String, name: String) {
+        
+        auth.createUser(withEmail: email, password: password) { authResult, error in
+            
+            if let error = error {
+                print("Error: \(error.localizedDescription)")
+                return
+            }
+            
+            guard let authResult = authResult else { return }
+            
+            // Lägg till newUserData
+            let newUserData = UserData(name: name, watchlist: [])
+            
+            do {
+                try self.db.collection(self.COLLECTION_USER_DATA).document(authResult.user.uid).setData(from: newUserData)
+            } catch _ {
+                print("Failed to create userdata!")
+            }
+            
+        }
+        
+        
+    } 
     
     
 }
