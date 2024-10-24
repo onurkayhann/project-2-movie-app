0aaedc61ec5bda9afa73c422f9d9f5d9456a0b32
Onur <onur.kayhan90@gmail.com>
2024-10-24 12:21:24 +0200

    Feature: Add MovieCard in Authenticated

--- FaveAndRate/Model/ApiModel.swift
+++ FaveAndRate/Model/ApiModel.swift
@@ -38,11 +38,13 @@ struct Movie: Codable, Identifiable {
         var id: String?
         var title: String?
         var year: Int?
+        var poster: String?
         
         enum CodingKeys: String, CodingKey {
             case id = "#IMDB_ID"
             case title = "#TITLE"
             case year = "#YEAR"
+            case poster = "#IMG_POSTER"
             // add more movie description later
         }
     }
---
+++ FaveAndRate/View/Authenticated/MovieCard.swift
@@ -0,0 +1,18 @@
+//
+//  MovieCard.swift
+//  FaveAndRate
+//
+//  Created by Onur Kayhan on 2024-10-24.
+//
+
+import SwiftUI
+
+struct MovieCard: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MovieCard()
+}
