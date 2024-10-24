4d6626b6670ecda97e6ed5aeb843b3a6771aa348
hampus-code <144155634+hampus-code@users.noreply.github.com>
2024-10-24 14:17:42 +0200

    Feature: Add moviecard homeview (#28)
    * Feature: Add MovieCard in HomeView
    
    * Feature: Add comment

--- FaveAndRate/View/Authenticated/HomeView.swift
+++ FaveAndRate/View/Authenticated/HomeView.swift
@@ -16,16 +16,31 @@ struct HomeView: View {
             //Rendering users name, adding title design such as font
             Text("Welcome, \(db.currentUserData?.name ?? "No user found")!")
 
+            ScrollView(.horizontal) {
                 
+                HStack(spacing: 15) {
+                    ForEach(movieManager.movies) { movies in
+                        MovieCard(movie: movies)
+                        
+                        // Make first image centered?
+                    }
+                }
+                
+            }
+            
+                /*
                 ForEach(movieManager.movies) { movieDescription in
+                    
                     
                     VStack {
                         
+                        /*
                         Text("\(movieDescription.title) (\(movieDescription.year))")
                                 .font(.headline)
+                         */
                     }
                 }
-            
+            */
             
             Button("Logout", action: {
                 //Signing out
